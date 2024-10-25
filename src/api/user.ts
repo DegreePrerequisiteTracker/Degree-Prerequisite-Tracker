171559673b0f98ecc2d5a772184ca4e0c44cdb4f
Nathan-Lackie <nlackie@calpoly.edu>
2024-10-25 10:56:21 -0700

    Create stubbed out User API

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,3 +2,23 @@ import express from "express";
 
 const router = express.Router();
 export default router;
+
+router.post("/user", (req, res) => {
+  res.send({
+    user_id: 5,
+  });
+});
+
+router.get("/user/history", (req, res) => {
+  res.send({
+    completed_courses: [12, 52, 236],
+  });
+});
+
+router.put("/user/history/:courseId", (req, res) => {
+  res.send();
+});
+
+router.delete("/user/history/:courseId", (req, res) => {
+  res.send();
+});
