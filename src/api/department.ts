4a8f4d781dcadb97c35d1ec8855e528ac84f2f76
Meboy1000 <Meboy10000@gmail.com>
2024-10-26 16:02:17 -0700

    fixed things

--- src/api/department.ts
+++ src/api/department.ts
@@ -6,29 +6,29 @@ export default router;
 router.get("/department", (req, res) => {
   res.send([
     {
-    "name": "Computer Science",
-    "department_id" : "CSC",
+      name: "Computer Science",
+      department_id: "CSC",
     },
     {
-      "name": "Computer Engineering",
-      "department_id" : "CPE",
+      name: "Computer Engineering",
+      department_id: "CPE",
     },
   ]);
 });
 
 router.get("/department/:department_id", (req, res) => {
   res.send({
-    "name": "Computer Science",
-    "department_id": "CSC",
+    name: "Computer Science",
+    department_id: "CSC",
   });
 });
 
 router.get("/department/:department_id/programs", (req, res) => {
   res.send([
     {
-      "program_id": 1,
-      "name": "Computer Science",
-      "program_types": ["BS", "MS", "Minor"],
+      program_id: 1,
+      name: "Computer Science",
+      program_types: ["BS", "MS", "Minor"],
     },
   ]);
 });
@@ -36,14 +36,14 @@ router.get("/department/:department_id/programs", (req, res) => {
 router.get("/department/:department_id/course", (req, res) => {
   res.send([
     {
-      "course_id": 1,
-      "course_name": "Fundamentals of Computer Science",
-      "course_number": 101,
+      course_id: 1,
+      course_name: "Fundamentals of Computer Science",
+      course_number: 101,
     },
     {
-      "course_id": 2,
-      "course_name": "Data Structures",
-      "course_number": 202,
+      course_id: 2,
+      course_name: "Data Structures",
+      course_number: 202,
     },
   ]);
 });
--- src/api/program.ts
+++ src/api/program.ts
@@ -6,55 +6,49 @@ export default router;
 router.get("/program", (req, res) => {
   res.send([
     {
-      "program_id": 1,
-      "name": "Computer Science",
-      "program_types": ["BS", "MS", "Minor",],
+      program_id: 1,
+      name: "Computer Science",
+      program_types: ["BS", "MS", "Minor"],
     },
     {
-      "program_id": 2,
-      "name": "Software Engineering",
-      "program_types": ["BS",],
+      program_id: 2,
+      name: "Software Engineering",
+      program_types: ["BS"],
     },
   ]);
 });
 
 router.get("/program/:program_id", (req, res) => {
-  res.send(
-    {
-      "program_id": 1,
-      "name": "Computer Science",
-      "program_types": ["BS", "MS", "Minor"],
-    }
-  );
+  res.send({
+    program_id: 1,
+    name: "Computer Science",
+    program_types: ["BS", "MS", "Minor"],
+  });
 });
 
 router.get("/program/:program_id/:program_type", (req, res) => {
-  res.send(
-    {
-      "name": "Computer Science",
-      "requirements": [1,2,3,4],
-    }
-  );
+  res.send({
+    name: "Computer Science",
+    requirements: [1, 2, 3, 4],
+  });
 });
 
 router.get("/program/:program_id/BS/concentration", (req, res) => {
   res.send([
     {
-      "concentration_id": 1,
-      "name": "Data Engineering",
+      concentration_id: 1,
+      name: "Data Engineering",
     },
     {
-      "concentration_id": 2,
-      "name": "Artificial Intelligence and Machine Learning",
+      concentration_id: 2,
+      name: "Artificial Intelligence and Machine Learning",
     },
   ]);
 });
 
 router.get("/program/:program_id/BS/concentration/:concentration_id", (req, res) => {
-  res.send(
-    {
-      "name": "Data Engineering",
-      "required_courses": [1,2,3,4,],
-    }
-  );
+  res.send({
+    name: "Data Engineering",
+    required_courses: [1, 2, 3, 4],
+  });
 });
