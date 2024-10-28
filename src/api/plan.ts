95631f612ddda065b301c4aa65bfda212bd386dd
zkanu <zoilanurikanu@gmail.com>
2024-10-28 10:24:19 -0700

    updated

--- src/api/plan.ts
+++ src/api/plan.ts
@@ -3,60 +3,50 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-router.get('/plan', (req, res) => {
+router.get("/plan", (req, res) => {
   res.send({
     plan_id: 1,
     program_id: 4505,
     program_type: "MS",
-    concentration_id: 3
+    concentration_id: 3,
   });
 });
 
-
-router.post('/plan', (req, res) => {
+router.post("/plan", (req, res) => {
   res.send({
-    "plan_id": 5
+    plan_id: 5,
   });
 });
 
-
-router.get('/plan/:plan_id', (req, res) =>{
+router.get("/plan/:plan_id", (req, res) => {
   res.send({
-    
-      "program_id": 3,
-      "program_type": "MS",
-      "concentration_id": 7
-    
-
+    program_id: 3,
+    program_type: "MS",
+    concentration_id: 7,
   });
 });
 
-
-router.put('/plan/:plan_id', (req,res) =>{
+router.put("/plan/:plan_id", (req, res) => {
   res.send();
 });
 
-
-router.delete('/plan/:plan_id',(req, res) => {
+router.delete("/plan/:plan_id", (req, res) => {
   res.send();
 });
 
-
-router.get('/plan/:plan_id',(req, res ) => {
+router.get("/plan/:plan_id", (req, res) => {
   res.send({
-    "name": "string", 
-    "course_number": 4056, 
-    "units": 4, 
-    "crosslisted": 53, 
-    "prerequisites": [
+    name: "string",
+    course_number: 4056,
+    units: 4,
+    crosslisted: 53,
+    prerequisites: [
       {
-        "needed": 3, 
-        "courses:": [12, 52, 236] 
-      }
+        needed: 3,
+        "courses:": [12, 52, 236],
+      },
     ],
-    "offered": ["F", "W", "SP", "SU"], 
-    "description": "course description",
-  
-  
+    offered: ["F", "W", "SP", "SU"],
+    description: "course description",
   });
 });
