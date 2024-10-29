01188fe9043ecda1239893f0635d943e3d2cc27a
KaiSnorlax <101308217+KaiiSnorlax@users.noreply.github.com>
2024-10-28 23:44:04 -0700

    Implement courses endpoint (#30)

--- src/api/courses.ts
+++ src/api/courses.ts
@@ -1,23 +1,25 @@
 import express from "express";
+import sql, { one } from "../database.js";
 
 const router = express.Router();
 export default router;
 
-router.get("/courses/:courseId", (req, res) => {
+interface Course {
+  prefix: string;
+  name: string;
+  number: number;
+  description: string;
+}
+
+router.get("/courses/:courseId", async (req, res) => {
+  const courseId = req.params.courseId;
+
+  const course = await one(sql<Course[]>`SELECT prefix, number, name, description FROM courses WHERE id = ${courseId}`);
+
   res.send({
-    coursePrefix: "CSC",
-    courseNumber: 365,
-    name: "Introduction to Database Systems",
-    units: 4,
-    crosslisted: null,
-    prerequisites: [
-      {
-        needed: 1,
-        courses: [20891, 21892, [11223, 12381, 12389], [231123, 88312, 192834]], // the course id(s) in DB
-      },
-    ],
-    offered: ["F", "W", "SP"],
-    description:
-      "Basic principles of database management systems (DBMS) and of DBMS application development. DBMS objectives, systems architecture, database models with emphasis on Entity-Relationship and Relational models, data definition and manipulation languages, the Structured Query Language (SQL), database design, application development tools. Course may be offered in classroom-based or online format. 3 lectures, 1 laboratory.",
+    prefix: course.prefix,
+    number: course.number,
+    name: course.name,
+    description: course.description,
   });
 });
