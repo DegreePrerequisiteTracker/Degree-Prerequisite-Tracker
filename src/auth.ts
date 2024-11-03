edb9e5f6508b36ba6a0f26418e0d9207caa73637
Nathan Lackie <nlackie@calpoly.edu>
2024-11-02 19:35:59 -0700

    Setup Authentication

---
+++ src/auth.ts
@@ -0,0 +1,41 @@
+import type { Request } from "express";
+import createHttpError from "http-errors";
+
+const supabaseUrl = process.env.SUPABASEURL!;
+const apiKey = process.env.SUPABASEKEY!;
+
+export interface UserData {
+  id: string;
+  email: string;
+}
+
+type AuthResponse =
+  | { id: string; email: string }
+  | {
+      code: number;
+      error_code: string;
+      msg: string;
+    };
+
+export async function authUser(req: Request): Promise<UserData> {
+  const auth = req.get("Authorization");
+
+  if (!auth) throw createHttpError.Unauthorized();
+
+  const result = (await fetch(`${supabaseUrl}/auth/v1/user`, {
+    method: "GET",
+    headers: {
+      Authorization: auth,
+      Apikey: apiKey,
+    },
+  }).then((res) => res.json())) as AuthResponse;
+
+  if ("code" in result) {
+    throw createHttpError(result.code, result.msg);
+  }
+
+  return {
+    id: result.id,
+    email: result.email,
+  };
+}
