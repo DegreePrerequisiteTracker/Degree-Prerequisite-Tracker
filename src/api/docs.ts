483dd075509ec22145eff6d99947beba12df707b
kai <kaiswangler@gmail.com>
2024-11-06 12:09:12 -0800

    Add docs page

---
+++ src/api/docs.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
+import fs from "fs";
+import { dirname } from "path";
+
+const router = express.Router();
+export default router;
+
+const appDir = dirname(import.meta.filename);
+const file = fs.readFileSync(`${appDir}/../../docs/apiSpec.yml`, "utf8");
+const swaggerDocument = YAML.parse(file) as object;
+
+router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,11 @@ import courses from "./api/courses.js";
 import programs from "./api/programs.js";
 import plans from "./api/plans.js";
 import departments from "./api/departments.js";
-import apiDocs from "./api/apiDocs.js";
+import docs from "./api/docs.js";
 
 export function createRoutes(app: Express) {
   // New routes need to be added to this array in order to work
-  const routes = [users, subjects, courses, programs, plans, apiDocs, departments];
+  const routes = [users, subjects, courses, programs, plans, docs, departments];
 
   routes.forEach((route) => app.use(route));
 }
