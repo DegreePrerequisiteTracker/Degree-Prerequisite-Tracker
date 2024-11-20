a6a645d82b4b4941c64a0e28be5fa9eb6a9baa4d
zkanu <zoilanurikanu@gmail.com>
2024-11-19 21:26:37 -0800

    resolved eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,27 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+export default tseslint.config(
+  {
+    ignores: ["**/*.js", "**/*.mjs"],
+  },
+  {
+    rules: {
+      eqeqeq: "error",
+      camelcase: "error",
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
