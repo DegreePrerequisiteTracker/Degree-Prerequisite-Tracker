2cd4e7e71d3afa9dc667fa2d266c96cf6ad3a54a
Kai <kaiswangler@gmail.com>
2024-11-28 22:13:11 -0800

    Migrate departments to new DB

--- src/api/departments.ts
+++ src/api/departments.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import sql, { one } from "../database.js";
+import sql from "../database.js";
 
 const router = express.Router();
 export default router;
@@ -7,44 +7,32 @@ export default router;
 interface Department {
   id: number;
   name: string;
-  college_prefix: string;
 }
 
 interface Program {
   id: number;
+  department_id: number;
   name: string;
   type: string;
 }
 
 router.get("/departments", async (req, res) => {
-  const departments = await sql<Department[]>`SELECT id, name, college_prefix FROM departments`;
+  const departments = await sql<Department[]>`
+    SELECT id, name FROM departments
+  `;
 
   res.send(
     departments.map((department) => ({
       id: department.id,
       name: department.name,
-      college: department.college_prefix,
     })),
   );
-});
-
-router.get("/departments/:departmentId", async (req, res) => {
-  const departmentId = req.params.departmentId;
-
-  const department = await one<Omit<Department, "id">>(
-    sql`SELECT name, college_prefix FROM departments WHERE id = ${departmentId}`,
-  );
-
-  res.send({
-    name: department.name,
-    college: department.college_prefix,
-  });
 });
 
 router.get("/departments/:departmentId/programs", async (req, res) => {
   const departmentId = req.params.departmentId;
 
-  const programs = await sql<Program[]>`
+  const programs = await sql<Pick<Program, "id" | "name" | "type">[]>`
     SELECT id, name, type FROM programs
     WHERE department_id = ${departmentId}
   `;
