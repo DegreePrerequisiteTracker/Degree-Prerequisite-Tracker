6eb1aad8e53a3cae8ef22733264b396a90952c64
Kai <kaiswangler@gmail.com>
2024-11-28 23:33:48 -0800

    Migrate programs to new DB

--- src/api/programs.ts
+++ src/api/programs.ts
@@ -1,37 +1,27 @@
 import express from "express";
-import sql, { one } from "../database.js";
+import sql from "../database.js";
+import createHttpError from "http-errors";
 
 const router = express.Router();
 export default router;
 
-interface Program {
-  name: string;
-  type: string;
-  department_id: number;
-}
-
 interface Concentration {
   id: number;
+  program_id: number;
   name: string;
 }
 
-router.get("/programs/:programId", async (req, res) => {
-  const programId = req.params.programId;
-  const program = await one<Program>(sql`SELECT id, name, type, department_id FROM programs WHERE id = ${programId}`);
-
-  res.send({
-    name: program.name,
-    type: program.type,
-    departmentId: program.department_id,
-  });
-});
-
 router.get("/programs/:programId/concentrations", async (req, res) => {
-  const programId = req.params.programId;
+  const programId = Number(req.params.programId);
 
-  const concentrations = await sql<
-    Concentration[]
-  >`SELECT id, name FROM concentrations WHERE program_id = ${programId}`;
+  if (isNaN(programId)) {
+    throw createHttpError.BadRequest("Program ID should be a number");
+  }
+
+  const concentrations = await sql<Concentration[]>`
+    SELECT id, name FROM concentrations
+    WHERE program_id = ${programId}
+  `;
 
   res.send(concentrations.map((concentration) => ({ id: concentration.id, name: concentration.name })));
 });
