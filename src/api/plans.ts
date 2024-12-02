8d3f746e6b7e27f01175073e65b7bf463e3b3418
Nathan-Lackie <nlackie@calpoly.edu>
2024-12-02 01:36:13 -0800

    Migrate plans endpoints to new schema

--- src/api/plans.ts
+++ src/api/plans.ts
@@ -20,15 +20,12 @@ interface PlanSummary {
   concentration_name: string;
 }
 
-interface PlanInfo {
-  course: number;
-  set: number | null;
-  prereq: number | null;
-}
-
 interface CourseInfo {
-  course: number;
-  prerequisites: number[][];
+  id: number;
+  prefix: string;
+  number: number;
+  name: string;
+  prerequisites: string[];
 }
 
 router.get("/plans", async (req, res) => {
@@ -36,8 +33,8 @@ router.get("/plans", async (req, res) => {
 
   const plans = await sql<
     PlanSummary[]
-  >`SELECT plans.id, graduation_date, programs.name AS program_name, concentrations.name AS concentration_name
-    FROM plans 
+  >`SELECT user_plans.id, graduation_date, programs.name AS program_name, concentrations.name AS concentration_name
+    FROM user_plans
     JOIN programs ON programs.id = program_id
     JOIN concentrations ON concentrations.id = concentration_id
     WHERE user_id = ${user.id}`;
@@ -62,7 +59,7 @@ router.post("/plans", async (req, res) => {
   const planBody = planReqBody.parse(req.body);
 
   const planId = await one<{ id: number }>(
-    sql`INSERT INTO plans (user_id, graduation_date, concentration_id, program_id) VALUES (${user.id}, ${planBody.graduationDate}, ${planBody.concentrationId}, ${planBody.programId}) RETURNING id`,
+    sql`INSERT INTO user_plans (user_id, graduation_date, concentration_id, program_id) VALUES (${user.id}, ${planBody.graduationDate}, ${planBody.concentrationId}, ${planBody.programId}) RETURNING id`,
   );
   res.send({
     planId: planId.id,
@@ -98,13 +95,18 @@ router.put("/plans/:planId", async (req, res) => {
   const planId = req.params.planId;
   const planBody = planReqBody.partial().parse(req.body);
 
-  await sql`
-    UPDATE plans SET
+  const result = await oneOrNull(sql`
+    UPDATE user_plans SET
     graduation_date = COALESCE(${planBody.graduationDate ?? null}, graduation_date),
     concentration_id = COALESCE(${planBody.concentrationId ?? null}, concentration_id),
     program_id = COALESCE(${planBody.programId ?? null}, program_id)
     WHERE user_id = ${user.id} AND id = ${planId}
-  `;
+    RETURNING id
+  `);
+
+  if (!result) {
+    throw createHttpError.NotFound(`No plan with ID ${req.params.planId}`);
+  }
 
   res.send();
 });
@@ -113,60 +115,66 @@ router.delete("/plans/:planId", async (req, res) => {
   const user = await authUser(req);
   const planId = req.params.planId;
 
-  await sql`DELETE FROM plans WHERE user_id = ${user.id} AND id = ${planId}`;
+  const result = await oneOrNull(sql`
+    DELETE FROM user_plans WHERE user_id = ${user.id} AND id = ${planId}
+    RETURNING id
+  `);
+
+  if (!result) {
+    throw createHttpError.NotFound(`No plan with ID ${req.params.planId}`);
+  }
+
   res.send();
 });
 
 router.get("/plans/:planId/courses", async (req, res) => {
   const user = await authUser(req);
 
-  const planCourses = await sql<PlanInfo[]>`
-    WITH recursive R AS(
-    SELECT program_courses.course_id as course FROM plans
-    JOIN program_courses ON plans.program_id = program_courses.program_id
-    WHERE plans.id = ${req.params.planId} AND plans.user_id = ${user.id}
-    UNION ALL
-    SELECT prerequisite_course_sets.course_id AS course FROM R
-    JOIN course_prerequisites ON course_prerequisites.course_id = course
-    JOIN prerequisite_course_sets ON course_prerequisites.set_number = prerequisite_course_sets.set_number 
-  )
-  SELECT Distinct course, course_prerequisites.set_number AS set, prerequisite_course_sets.course_id as prereq FROM R
-  left JOIN course_prerequisites ON course = course_prerequisites.course_id
-  left JOIN prerequisite_course_sets ON course_prerequisites.set_number = prerequisite_course_sets.set_number
-  ORDER BY course, set`;
+  const planCourses = await sql<CourseInfo[]>`
+    WITH RECURSIVE plan_course_sets AS (
+      -- Get all courses directly required by the program
+      SELECT DISTINCT program_requirement_courses.course_set_number FROM user_plans
+      JOIN program_requirements ON user_plans.program_id = program_requirements.program_id
+      JOIN program_requirement_sets ON program_requirements.requirement_group = program_requirement_sets.requirement_group
+      JOIN program_requirement_courses ON program_requirement_sets.set_number = program_requirement_courses.set_number
+      WHERE user_plans.id = ${req.params.planId} AND user_plans.user_id = ${user.id}
+      UNION
+      -- Recursively get all of their prerequisites
+      SELECT DISTINCT course_requisite_sets.course_set_number FROM course_requisites
+      JOIN plan_course_sets ON course_requisites.course_set_number = plan_course_sets.course_set_number
+      JOIN course_requisite_sets ON course_requisites.set_number = course_requisite_sets.set_number
+    ),
+    primary_courses AS (
+      -- Remove crosslisted courses
+      SELECT DISTINCT ON (set_number) id, prefix, number, name, set_number FROM courses
+    ),
+    prereq_groups AS (
+      -- Create prerequisite groups for all plan courses
+      SELECT primary_courses.id, primary_courses.prefix, primary_courses.number, primary_courses.name, course_requisite_sets.set_number, STRING_AGG(prereqs.id::text, ',') AS prereq_group FROM plan_course_sets
+      JOIN primary_courses ON plan_course_sets.course_set_number = primary_courses.set_number
+      JOIN course_requisites ON plan_course_sets.course_set_number = course_requisites.course_set_number
+      JOIN course_requisite_sets ON course_requisites.set_number = course_requisite_sets.set_number
+      JOIN primary_courses AS prereqs ON course_requisite_sets.course_set_number = prereqs.set_number
+      GROUP BY primary_courses.id, primary_courses.prefix, primary_courses.number, primary_courses.name, course_requisite_sets.set_number
+    )
+    -- Aggregate prerequisite groups into arrays
+    SELECT id, prefix, number, name, ARRAY_AGG(prereq_group) AS prerequisites FROM prereq_groups
+    GROUP BY id, name, prefix, number
+    ORDER BY prefix, number
+  `;
 
-  if (planCourses.length === 0) {
-    throw createHttpError.NotFound(`No plan with ID ${req.params.planId}`);
-  }
-  const plan: CourseInfo[] = [];
-  let coursenum: number = planCourses[0].course;
-  let prevset: number | null = planCourses[0].set;
-  let prereqGroup: number[][] = [];
-  let prereqs: number[] = [];
-  planCourses.forEach((element) => {
-    if (element.course !== coursenum) {
-      prereqGroup.push(prereqs);
-      plan.push({
-        course: coursenum,
-        prerequisites: prereqGroup,
-      });
-      coursenum = element.course;
-      prereqGroup = [];
-      prereqs = [];
-      prevset = element.set;
-    } else if (element.set !== prevset) {
-      prereqGroup.push(prereqs);
-      prevset = element.set;
-      prereqs = [];
-    }
-    if (element.prereq !== null && element.set !== null) {
-      prereqs.push(element.prereq);
-    }
-  });
-  prereqGroup.push(prereqs);
-  plan.push({
-    course: coursenum,
-    prerequisites: prereqGroup,
-  });
-  res.send(plan);
+  const planCoursesTwo = planCourses.map((course) => ({
+    ...course,
+    prerequisites: course.prerequisites.map((group) => group.split(",").map((courseId) => Number(courseId))),
+  }));
+
+  res.send(
+    planCoursesTwo.map((course) => ({
+      courseId: course.id,
+      prefix: course.prefix,
+      number: course.number,
+      name: course.name,
+      prerequisites: course.prerequisites,
+    })),
+  );
 });
